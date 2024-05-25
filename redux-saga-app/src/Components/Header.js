468775bea0761b474752266e866940a4290c63c8
gohil rohit <gohilrohit309@gmail.com>
2024-05-25 14:33:56 +0530

    apply react-bootstrap and make ui

--- redux-saga-app/src/Components/Header.js
+++ redux-saga-app/src/Components/Header.js
@@ -2,29 +2,58 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { productSearch } from "../Redux/ProductAction"
-
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
 
     const result = useSelector((state) => state.cartData);
     console.log(result);
     const dispatch = useDispatch()
-       
+
     return (
         <div className="header">
-            <Link to={"/"}>
-                <h1 style={{ float: "left", marginLeft: "24px" }}>Logo</h1>
-            </Link>
-            <div className="serchbox">
-                    <input type="text" name="search" placeholder="Search" onChange={(e)=>dispatch(productSearch(e.target.value))} />
-                </div>
-            <Link to={"/cart"}>
-                
-                <div className="cart-div">
-                    <span>{result.length}</span>
-                    <img src="https://cdn-icons-png.freepik.com/256/1170/1170678.png?semt=ais_hybrid" />
-                </div>
-            </Link>
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container fluid>
+                    <Navbar.Brand href="#">
+                        <Link to={"/"}>
+                            <h1 style={{ float: "left", marginLeft: "24px" }}>Logo</h1>
+                        </Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Nav.Link href="#action1">Home</Nav.Link>
+                            <Nav.Link href="#action2">About</Nav.Link>
+                            <Nav.Link href="#action2">Services</Nav.Link>
+                            <Nav.Link href="#action2">Contact</Nav.Link>
+                        </Nav>
+                        <Form className="d-flex">
+                            <Form.Control
+                                type="search"
+                                placeholder="Search"
+                                className="me-3"
+                                aria-label="Search"
+                                name="search"
+                                onChange={(e) => dispatch(productSearch(e.target.value))}
+                            />
+                        </Form>
+                    </Navbar.Collapse>
+                    <Link to={"/cart"}>
+                        <div className="cart-div">
+                            <span>{result.length}</span>
+                            <img src="https://cdn-icons-png.freepik.com/256/1170/1170678.png?semt=ais_hybrid" />
+                        </div>
+                    </Link>
+                </Container>
+            </Navbar>
         </div>
     )
 }
--- redux-saga-app/src/Components/Main.js
+++ redux-saga-app/src/Components/Main.js
@@ -2,39 +2,58 @@ import { useDispatch, useSelector } from 'react-redux';
 import { addToCart, emptyCart, removeToCart } from '../Redux/Action';
 import { productListing } from '../Redux/ProductAction';
 import { useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function Main() {
 
     const dispatch = useDispatch();
     const data = useSelector((state) => state.productData);
     console.log(data)
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(productListing())
-    },[])
+    }, [])
 
     return (
         <>
-            <div>
-                <button onClick={() => dispatch(emptyCart())} >Empty To Cart</button>
+            <div className='mt-5'>
+                <Button variant="danger" onClick={() => dispatch(emptyCart())} >Empty To Cart</Button>
             </div>
             <div className='productContainer'>
-                {data.map((v, i) => {
-                    return (
-                        <div className='product-items' key={i}>
-                            <img src={v.photo} />
-                            <div className='productName'>Name :- {v.name}</div>
-                            <div className='productPrice'>Price :- {v.price}</div>
-                            <div className='productQuantity'>Quantity :- {v.quantity}</div>
-                            <div className='productTotalPrice'>Category :- {v.category}</div>
-                            <div className='productId'>Brand :- {v.brand}</div>
-                            <div>
-                                <button onClick={() => dispatch(addToCart(v))}>Add To Cart</button>  ||   
-                                <button onClick={() => dispatch(removeToCart(v.id))} >Remove</button>
-                            </div>
-                        </div>
-                    )
-                })}
+                <Container>
+                    <Row className="justify-content-md-center">
+                        {data.map((v, i) => {
+                            return (
+                                <Col md="3" className='mt-5'>
+                                    <Card border="success"  style={{ width: '18rem' }}>
+                                        <Card.Img variant="top" src={v.photo} style={{height:"280px" , objectFit:"cover"}} />
+                                        <Card.Body>
+                                            <Card.Title>Name :- {v.name}</Card.Title>
+                                            <Card.Text>
+                                                <div className='productPrice'>Price :- {v.price}</div>
+                                                <div className='productQuantity'>Quantity :- {v.quantity}</div>
+                                                <div className='productTotalPrice'>Category :- {v.category}</div>
+                                                <div className='productId'>Brand :- {v.brand}</div>
+                                            </Card.Text>
+                                            <div>
+                                                <Button variant="primary" onClick={() => dispatch(addToCart(v))}>Add To Cart</Button>
+                                                <Button variant="secondary" className='ms-3' onClick={() => dispatch(removeToCart(v.id))} >Remove</Button>
+                                            </div>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            )
+                        })}
+                    </Row>
+                </Container>
             </div>
+
         </>
     );
 }
